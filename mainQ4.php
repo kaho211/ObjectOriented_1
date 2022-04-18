49104c2be85935e2648ed823bbb057e99cac7137
kaho211 <kaho.fujii@tokyo.supersoftware.co.jp>
2022-04-18 12:57:59 +0900

    first commit

---
+++ mainQ4.php
@@ -0,0 +1,16 @@
+<?php
+require_once 'Classes\Cars\Cars.php';
+require_once 'Classes\Cars\Honda.php';
+require_once 'Classes\Cars\Nissan.php';
+require_once 'Classes\Cars\Ferrari.php';
+
+$nissan = new Nissan();
+echo "[車の仕様]\n";
+$nissan->printCarDetail();
+echo "\n～乗車中～\n";
+$nissan->getOn(1);
+$nissan->getOn(3);
+echo "\n～降車中～\n";
+$nissan->getOff(1);
+$nissan->getOff(1);
+?>
